716d5a5b8c9dcb02bb6d14166e2f27ad9512c5f8
jinnoco <k.jin3217@gmail.com>
2024-08-13 10:52:40 +0900

    fix layout

--- app/src/main/java/com/example/jetpack_compose_tutorial/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_compose_tutorial/MainActivity.kt
@@ -3,41 +3,69 @@ package com.example.jetpack_compose_tutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetpack_compose_tutorial.ui.theme.JetpackcomposetutorialTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackcomposetutorialTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    MessageCard(sample_message)
                 }
             }
         }
     }
 }
 
+data class Message(val author: String, val body: String)
+private val sample_message = Message("Jin", "Hello Jetpack Compose")
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MessageCard(message: Message) {
+
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.sample_image_1),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+            )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column {
+            Text(text = message.author)
+            Spacer(modifier = Modifier.width(4.dp))
+            Text(text = message.body)
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     JetpackcomposetutorialTheme {
-        Greeting("Android")
+        MessageCard(sample_message)
     }
 }
