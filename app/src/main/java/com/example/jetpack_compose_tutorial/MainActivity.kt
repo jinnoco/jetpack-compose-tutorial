2fc76d662819b06d8d30b262218bb31bc1a171a6
jinnoco <k.jin3217@gmail.com>
2024-08-13 13:43:01 +0900

    improve UI with material design

--- app/src/main/java/com/example/jetpack_compose_tutorial/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_compose_tutorial/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.jetpack_compose_tutorial
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -21,15 +23,15 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.jetpack_compose_tutorial.ui.theme.JetpackcomposetutorialTheme
+import com.example.jetpack_compose_tutorial.ui.theme.JetpackComposeTutorialTheme
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackcomposetutorialTheme {
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+            JetpackComposeTutorialTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
                     MessageCard(sample_message)
                 }
             }
@@ -49,23 +51,43 @@ fun MessageCard(message: Message) {
             modifier = Modifier
                 .size(40.dp)
                 .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
             )
 
         Spacer(modifier = Modifier.width(8.dp))
 
         Column {
-            Text(text = message.author)
+            Text(
+                text = message.author,
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.titleSmall
+            )
+
             Spacer(modifier = Modifier.width(4.dp))
-            Text(text = message.body)
+
+            Surface(shape = MaterialTheme.shapes.medium, shadowElevation = 1.dp) {
+                Text(
+                    text = message.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+
         }
     }
 
 }
 
-@Preview(showBackground = true)
-@Composable
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)@Composable
 fun GreetingPreview() {
-    JetpackcomposetutorialTheme {
-        MessageCard(sample_message)
+    JetpackComposeTutorialTheme {
+        Surface {
+            MessageCard(sample_message)
+        }
     }
 }
